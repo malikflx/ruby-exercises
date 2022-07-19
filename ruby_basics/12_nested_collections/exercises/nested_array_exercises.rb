66f6df630db50c9296943dd64fb53c56d3647098
Malik Felix <malikfelix50@gmail.com>
2022-07-18 22:16:08 -0400

    completing nested array exercises

--- ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_array_exercises.rb
@@ -11,16 +11,21 @@ def blank_seating_chart(number_of_rows, seats_per_row)
 
   # NOTE: if one of the nested arrays is changed, the others should **not**
   # change with it
+  Array.new(number_of_rows) { Array.new(seats_per_row) }
 end
 
 def add_seat_to_row(chart, row_index, seat_to_add)
   # take a chart (2d array)  and add seat_to_add to the end of the row that is
   # at row_index index of the chart, then return the chart
+  chart[row_index].push(seat_to_add)
+  return chart
 end
 
 def add_another_row(chart, row_to_add)
   # take a chart and add row_to_add to the end of the chart,
   # then return the chart.
+  chart.push(row_to_add)
+  return chart
 end
 
 def delete_seat_from_row(chart, row_index, seat_index)
@@ -28,17 +33,28 @@ def delete_seat_from_row(chart, row_index, seat_index)
   # the chart, then return the chart
 
   # Hint: explore the ruby docs to find a method for deleting from an array!
+  chart[row_index].delete_at(seat_index)
+  return chart
 end
 
 def delete_row_from_chart(chart, row_index)
   # take a chart and delete the row at row_index of the chart,
   # then return the chart
+  chart.delete_at(row_index)
+  return chart
 end
 
 def count_empty_seats(chart)
   # take a chart and return the number of empty (nil) seats in it
 
   # NOTE: `chart` should **not** be mutated
+  empty_seats = 0
+  chart.flatten.each do |seat|
+    if seat.nil?
+      empty_seats = empty_seats + 1
+    end
+  end
+  return empty_seats
 end
 
 def find_favorite(array_of_hash_objects)
@@ -55,4 +71,13 @@ def find_favorite(array_of_hash_objects)
 
   # TIP: there will only be a maximum of one hash in the array that will
   # return true to the :is_my_favorite? key
+  # array_of_hash_objects.filter_map { |data| data if data[:is_my_favorite?] }
+  array_of_hash_objects.each do | hash |
+    hash.each do |name, data|
+      if name == :is_my_favorite? && data == true
+        return hash
+      end
+    end
+  end
+  return nil
 end
